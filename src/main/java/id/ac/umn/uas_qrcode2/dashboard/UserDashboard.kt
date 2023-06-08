9e285042391c5cfad7a6a292721a7e9519bb9ce7
Audd790 <56447381+Audd790@users.noreply.github.com>
2023-06-08 13:04:29 +0700

    ganti nama package

---
+++ src/main/java/id/ac/umn/uas_qrcode2/dashboard/UserDashboard.kt
@@ -0,0 +1,4 @@
+package id.ac.umn.uas_qrcode2.dashboard
+
+class UserDashboard {
+}
---
+++ src/main/java/id/ac/umn/uas_qrcode2/sesion/user.kt
@@ -0,0 +1,3 @@
+package id.ac.umn.uas_qrcode2.sesion
+
+data class User(val username: String, val password: String)

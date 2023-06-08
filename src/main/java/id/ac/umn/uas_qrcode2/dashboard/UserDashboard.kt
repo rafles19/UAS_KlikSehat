dd1185b2927eb47e94c27f2c31139f05630d2ac4
Audd790 <56447381+Audd790@users.noreply.github.com>
2023-06-08 13:06:58 +0700

    Delete src directory

--- src/main/java/id/ac/umn/uas_qrcode2/dashboard/UserDashboard.kt
+++
@@ -1,4 +0,0 @@
-package id.ac.umn.uas_qrcode2.dashboard
-
-class UserDashboard {
-}
--- src/main/java/id/ac/umn/uas_qrcode2/sesion/user.kt
+++
@@ -1,3 +0,0 @@
-package id.ac.umn.uas_qrcode2.sesion
-
-data class User(val username: String, val password: String)
